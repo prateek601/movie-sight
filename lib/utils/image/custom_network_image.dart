78118404523d9123d44de191a61df744370de2d6
Prateek Jain <jprateek601@gmail.com>
2022-06-30 00:14:46 +0530

    Created a custom widget for showing network images

---
+++ lib/utils/image/custom_network_image.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  final String image;
+  final double? height;
+  final double? width;
+  final BoxFit? fit;
+
+  const CustomNetworkImage({Key? key,
+    required this.image,
+    this.height,
+    this.width,
+    this.fit = BoxFit.cover,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+      height: height,
+      width: width,
+      fit: fit,
+      errorBuilder: (context, object, stackTrace) {
+        return Center(
+          child: Container(
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.grey[800]!, width: 2)
+            ),
+            child: const Icon(
+              Icons.photo,
+              color: Colors.white,
+              size: 38,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
