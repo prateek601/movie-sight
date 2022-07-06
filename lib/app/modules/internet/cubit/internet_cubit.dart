5094cbfc4291460a7ca8cb6f701f646caa84c9a9
Prateek Jain <jprateek601@gmail.com>
2022-07-06 17:37:08 +0530

    changes in internet cubit

--- lib/app/modules/internet/cubit/internet_cubit.dart
+++ lib/app/modules/internet/cubit/internet_cubit.dart
@@ -9,34 +9,29 @@ part 'internet_state.dart';
 class InternetCubit extends Cubit<InternetState> {
   final Connectivity connectivity = Connectivity();
   late StreamSubscription subscription;
+  bool firstTime = true;
 
   InternetCubit() : super(CheckingConnection()) {
-    checkConnectivity();
     subscribeConnectionStream();
-  }
-
-  Future<void> checkConnectivity() async {
-    var connectivityResult = await connectivity.checkConnectivity();
-    emitConnectionState(result: connectivityResult);
   }
 
   void subscribeConnectionStream() {
     subscription = connectivity.onConnectivityChanged.listen(
       (ConnectivityResult result) {
-        print('changed');
         emitConnectionState(result: result);
       },
     );
   }
 
   void emitConnectionState({required ConnectivityResult result}) {
-    if (result == ConnectivityResult.mobile) {
-      emitConnected();
-    } else if (result == ConnectivityResult.wifi) {
-      emitConnected();
-    } else {
+    if (result == ConnectivityResult.none) {
       emitDisconnected();
+    } else {
+      if(!firstTime) {
+        emitConnected();
+      }
     }
+    firstTime = false;
   }
 
   void cancelSubscription() {
